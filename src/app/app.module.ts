bff06b6e04182ab5274a3d4408e4bcca45817929
bhxrth <bharathcaptain888@gmail.com>
2022-07-07 20:17:06 +0530

    payment created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { SearchrestaurantComponent } from './searchrestaurant/searchrestaurant.component';
+import { ViewfoodComponent } from './viewfood/viewfood.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    SearchrestaurantComponent,
+    ViewfoodComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchrestaurant/searchrestaurant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchrestaurant',
+  templateUrl: './searchrestaurant.component.html',
+  styleUrls: ['./searchrestaurant.component.css']
+})
+export class SearchrestaurantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
